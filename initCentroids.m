53766108d22231d29cb4ac1e27683564ef2b87cc
HydroxideX <YahiaElsaadawy@gmail.com>
2020-03-19 12:09:54 +0200

    Initializing for adding a classifier

--- initCentroids.m
+++
@@ -1,5 +0,0 @@
-function centroids = initCentroids(X,K)
-  centroids = zeros(K, size(X, 2));
-  randidx = randperm(size(X, 1));
-  centroids = X(randidx(1:K), :);
-endfunction
--- main.m
+++ main.m
@@ -1,6 +1,3 @@
 X = readTrainingData();
 size(X)
-K = 2;
-max_iters = 10;
-initial_centroids = initCentroids(X, K);
-[centroids, idx] = runkMeans(X, initial_centroids, max_iters);
+??max_iters = 10;
